2bcd7f88e81624475bed10de22c665dbfb4997b4
Z. K. Edmondson <zack.edmondson@gmail.com>
2018-05-07 19:43:24 -0400

    Cleanup

--- Vis.hs
+++ Vis.hs
@@ -36,7 +36,3 @@ main = display
         (10, 10))       -- window position
         white                   -- background color
         (Pictures [pictureBack, pictureFore])                 -- picture to display
-
---picture=Translate (-170) (-20)    -- shift the text to the middle of the window
---    $ Scale 0.5 0.5             -- display it half the original size
---    $ Text "Hello World"    -- text to display
