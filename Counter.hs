a5d81a2d86b82c9a8bf60b99db216c16a41a38c3
Z. K. Edmondson <zack.edmondson@gmail.com>
2018-05-13 16:41:06 -0400

    Modified the Counter monad to provide a cleaner accessor, although this added significant additional complexity to the related functions.

--- Counter.hs
+++ Counter.hs
@@ -7,7 +7,10 @@ Maintainer  :
 Stability   : experimental
 
 Counter Monad, mainly so I can re-learn monads and avoid 
-overkill of using the State monad for this purpose.
+overkill of using the State monad for this purpose.  Note
+that this monad is basically the State monad where the state
+is restricted to Int and the accessor/mutator functions
+are increment and current
 
 -}
 
@@ -16,20 +19,40 @@ module Counter where
 import Control.Applicative
 import Control.Monad
 
-newtype Counter a = Counter {runCounter::(a, Int)} 
-    deriving (Show)
+newtype Counter a = Counter {runCounter::Int -> (a, Int)} 
+--    deriving (Show)
 
 instance Functor Counter where
-    fmap f = \(Counter (x,i)) -> Counter (f x, i) 
+    fmap f (Counter g) = 
+        Counter $ \i -> 
+            let (a, i1) = g i
+            in (f a, i1)
 
 instance Applicative Counter where
-    pure = \x -> Counter (x, 0)
-    liftA2 f = \(Counter (x,i)) -> (\(Counter (y,j)) -> Counter (f x y, i+j))
+    pure x = Counter $ \i -> (x,i)
+    liftA2 f (Counter g) (Counter h) = 
+        Counter $ \i -> 
+            let (a,i) = g i
+                (b,j) = h i
+            in (f a b, i+j)   
 
 instance Monad Counter where
-    (Counter (x,c)) >>= f = let (Counter (y,c')) = f x in Counter (y,(c+c'))
-    return x = Counter (x,0) 
+    return x = Counter $ \i -> (x, i)
+    (Counter g) >>= f = 
+        Counter $ \s -> let (a, i') = g s
+                            (Counter h) = f a
+                         in h i'
 
 increment :: Counter ()
-increment = Counter ((), 1)
+increment = Counter $ \i -> ((), i+1)
 
+current :: Counter Int
+current = Counter $ \i -> (i,i)
+
+testCounter = do
+    increment 
+    increment
+    a <- current
+    increment
+    increment
+    return a
